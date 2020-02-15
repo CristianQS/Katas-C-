b721ca9ff1820822f14be9587fe1b5fd377629bc
CristianQS <cristian_31teror@hotmail.com>
2020-02-15 20:06:16 +0000

    green: find_odd_number

---
+++ OddTimesNumber/OddTimesNumber.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq;
+
+namespace OddTimesNumber {
+    public class OddTimesNumber {
+        public OddTimesNumber() {
+        }
+
+        public int find_it(int[] seq)
+        {
+            var result = seq.Where((number) => appearTimes(number, seq) % 2 != 0);
+            return result.ToArray()[0];
+        }
+
+        public int appearTimes(int numberTarget, int[] seq)
+        {
+            var numberTargetArray = seq.Where(number => number == numberTarget);
+            return numberTargetArray.Count();
+        }
+    }
+}
---
+++ OddTimesNumber/OddTimesNumberShould.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace OddTimesNumber {
+    [TestFixture]
+      public class OddTimesNumberShould {
+          [Test]
+          public void Tests()
+          {
+              Assert.AreEqual(5, new OddTimesNumber().find_it(new[] { 20, 1, -1, 2, -2, 3, 3, 5, 5, 1, 2, 4, 20, 4, -1, -2, 5 }));
+          }
+      }
+
+}
