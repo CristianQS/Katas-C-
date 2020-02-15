ed2f58f2722681a95a511b35ed83f6404d0708f5
CristianQS <cristian_31teror@hotmail.com>
2020-02-15 13:31:25 +0000

    green: get_fizz_when_num_is_multiply_of_5

--- FizzBuzz/FizzBuzzClass.cs
+++ FizzBuzz/FizzBuzzClass.cs
@@ -1,4 +1,4 @@
-namespace FizzBuzz {
+﻿namespace FizzBuzz {
     public class FizzBuzzClass {
         private string[] arrayResult;
         public FizzBuzzClass() {
@@ -9,8 +9,17 @@ namespace FizzBuzz {
             for (int count = 0; count < arrayResult.Length; count++) {
                 arrayResult[count] = (count+1).ToString();
                 if(MultiplyOfThree(count)) arrayResult[count] = "Fizz";
+                if(MultiplyOfFive(count)) arrayResult[count] = "Buzz";
             }
             return arrayResult;
+        }
+
+        private bool MultiplyOfFive(int count) {
+            return (count + 1) % 5 == 0;
+        }
+
+        private static bool MultiplyOfThree(int count) {
+            return (count + 1) % 3 == 0;
         }
     }
 }
--- Test.Fizzbuzz/FizzBuzzShould.cs
+++ Test.Fizzbuzz/FizzBuzzShould.cs
@@ -17,10 +17,18 @@ namespace Test.Fizzbuzz
             var lengthArray = 3;
 
             var result = fizzBuzz.GetFizzBuzzArray(lengthArray);
-            var a = 1 % 3;
             var expected = new[] {"1", "2", "Fizz"};
             Assert.AreEqual(expected,result);
+        }        
+        
+        [Test]
+        public void get_fizz_when_num_is_multiply_of_5() {
+            var lengthArray = 10;
 
+            var result = fizzBuzz.GetFizzBuzzArray(lengthArray);
+
+            var expected = new[] {"1", "2", "Fizz","4","Buzz","Fizz","7","8","Fizz","Buzz"};
+            Assert.AreEqual(expected,result);
         }
     }
 }
