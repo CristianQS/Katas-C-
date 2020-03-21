ec07cfe5080af6330925ba192e33ffed794c6591
CristianQS <cristian_31teror@hotmail.com>
2020-03-21 02:16:14 +0000

    fix

--- MarsRoversKata/MarsRover/rover.cs
+++ MarsRoversKata/MarsRover/rover.cs
@@ -2,24 +2,21 @@ using System.Collections.Generic;
 
 namespace MarsRover {
     public class Rover {
-        private Point point;
-        private Directions direction;
+        public Point Point { get; }
+        public Directions Direction { get; set; }
 
         public Rover(Point point, Directions direction) {
-            this.point = point;
-            this.direction = direction;
+            this.Point = point;
+            this.Direction = direction;
         }
 
-        public Point Point => point;
-
-        public Directions Direction => direction;
 
         public void Execute(List<Commands> commandsList) {
             commandsList.ForEach(command => {
-                if (command.Equals(Commands.Forward)) this.point.y++;
-                if (command.Equals(Commands.Backward)) this.point.y--;
-                if (command.Equals(Commands.Right)) this.direction = Directions.East;
-                if (command.Equals(Commands.Left)) this.direction = Directions.West;
+                if (command.Equals(Commands.Forward)) this.Point.y++;
+                if (command.Equals(Commands.Backward)) this.Point.y--;
+                if (command.Equals(Commands.Right)) this.Direction = Directions.East;
+                if (command.Equals(Commands.Left)) this.Direction = Directions.West;
             });
         }
     }
