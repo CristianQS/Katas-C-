17c382ea15da5594a33ca4425b8dbd12b7c027b4
CristianQS <cristian_31teror@hotmail.com>
2020-03-22 20:08:23 +0000

    Introduce field variable

--- MarsRoversKata/MarsRover/Mars.cs
+++ MarsRoversKata/MarsRover/Mars.cs
@@ -4,20 +4,27 @@ namespace MarsRover {
     public class Mars : Planet {
         public int Latitude { get; set; }
         public int Longitude { get; set; }
+        public Dictionary<string, string> Map { get; set; }
+
 
         public Mars(int latitude, int longitude) {
             Latitude = latitude;
             Longitude = longitude;
+            Map = GenerateMap();
+        }
+
+        public Dictionary<string, string> GenerateMap() {
+            Map = new Dictionary<string, string>();
+            for (var longitudeIndex = -Longitude; longitudeIndex <= Longitude; longitudeIndex++) {
+                for (var latitudeIndex = Latitude; latitudeIndex >= -Latitude; latitudeIndex--) {
+                    Map.Add($"{latitudeIndex},{longitudeIndex}","-");
+                }
+            }
+            return Map;
         }
 
         public Dictionary<string, string> GetMap() {
-            var map = new Dictionary<string, string>();
-            for (var longitudeIndex = -Longitude; longitudeIndex <= Longitude; longitudeIndex++) {
-                for (var latitudeIndex = Latitude; latitudeIndex >= -Latitude; latitudeIndex--) {
-                    map.Add($"{latitudeIndex},{longitudeIndex}","-");
-                }
-            }
-            return map;
+            return Map;
         }
     }
 }
