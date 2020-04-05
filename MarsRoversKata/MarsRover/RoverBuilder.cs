7701f2e59062e28456df4781f00e65d1c9059f21
CristianQS <cristian_31teror@hotmail.com>
2020-04-05 01:47:14 +0100

    Create RoverBuilder

---
+++ MarsRoversKata/MarsRover/RoverBuilder.cs
@@ -0,0 +1,25 @@
+﻿using MarsRover.Enums;
+
+namespace MarsRover {
+    public class RoverBuilder {
+        private Planet Planet;
+        private Point Point;
+        private Directions Direction;
+
+        public void WithMars(int latitude, int longitude) {
+            Planet = new Mars(latitude, longitude);
+        }
+
+        public void WithPoint(int x, int y) {
+            Point = new Point(x,y);
+        }
+
+        public void WithDirections(Directions direction) {
+            Direction = direction;
+        }
+
+        public Rover Build() {
+            return new Rover(Point, Direction, Planet);
+        }
+    }
+}
